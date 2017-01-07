16d235f98233c1a2716e17832972326806066c77
Varya Stepanova <mail@varya.me>
2017-01-07 13:32:18 +0200

    Logo

--- src/templates/components/Header/index.js
+++ src/templates/components/Header/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+import Logo from "../Logo"
+
 import style from './style.css'
 
 export default class Header extends Component {
@@ -7,7 +9,11 @@ export default class Header extends Component {
   render() {
     return (
       <header className={style.header}>
-        I am header!
+        <div className={style.left}/>
+        <div className={style.center}>
+          <Logo/>
+        </div>
+        <div className={style.right}/>
       </header>
     )
 
